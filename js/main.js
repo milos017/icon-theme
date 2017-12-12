64b53bdf147855f4df37724b4d7e29e795314c94
Milos Stojiljkovic <milos017ffm@yahoo.com>
2017-12-12 16:26:16 +0100

    stars

--- js/main.js
+++ js/main.js
@@ -26,35 +26,74 @@ $(document).ready(function () {
         var scrool = $(window).scrollTop();
         $('.cont').each(function () {
             var position = $(this).offset().top;
-        if (position < vh + scrool - 100) {
-            var val = parseInt($(this).attr('data-pct'));
-            var $circle = $(this).find('.svg .bar');
+            if (position < vh + scrool - 100) {
+                var val = parseInt($(this).attr('data-pct'));
+                var $circle = $(this).find('.svg .bar');
 
 
 
-            var r = $circle.attr('r');
-            var c = Math.PI * (r * 2);
-        
+                var r = $circle.attr('r');
+                var c = Math.PI * (r * 2);
 
 
-        var circle = $(this).find('.svg .bar');
-    
 
-    
+                var circle = $(this).find('.svg .bar');
 
 
 
 
-        var pct = ((100 - val) / 100) * c;
 
-        $circle.css({strokeDashoffset: pct});
 
-        // $('#cont').attr('data-pct',val);
-    }
+
+                var pct = ((100 - val) / 100) * c;
+
+                $circle.css({strokeDashoffset: pct});
+
+                // $('#cont').attr('data-pct',val);
+            }
+        });
+
     });
-
-});
 });
 
-if($('.owl-carousel'))
+var currentRating = $('#example-fontawesome-o').data('current-rating');
+
+$('.stars-example-fontawesome-o .current-rating')
+        .find('span')
+        .html(currentRating);
+
+$('.stars-example-fontawesome-o .clear-rating').on('click', function (event) {
+    event.preventDefault();
+
+    $('#example-fontawesome-o')
+            .barrating('clear');
+});
+
+$('#example-fontawesome-o').barrating({
+    theme: 'fontawesome-stars-o',
+    showSelectedRating: false,
+    initialRating: currentRating,
+    onSelect: function (value, text) {
+        if (!value) {
+            $('#example-fontawesome-o')
+                    .barrating('clear');
+        } else {
+            $('.stars-example-fontawesome-o .current-rating')
+                    .addClass('hidden');
+
+            $('.stars-example-fontawesome-o .your-rating')
+                    .removeClass('hidden')
+                    .find('span')
+                    .html(value);
+        }
+    },
+    onClear: function (value, text) {
+        $('.stars-example-fontawesome-o')
+                .find('.current-rating')
+                .removeClass('hidden')
+                .end()
+                .find('.your-rating')
+                .addClass('hidden');
+    }
+});
 
